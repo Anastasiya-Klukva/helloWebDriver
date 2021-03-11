bf9561a94818d3755a83bc7d55906690246b1b9f
Anastasiya-Klukva <Petrushka_n@mail.ru>
2021-03-11 18:21:36 +0300

    Initial commit

---
+++ src/test/java/PastebinTestTest.java
@@ -0,0 +1,38 @@
+// I Can Win
+//        При выполнении задания необходимо использовать возможности Selenium WebDriver, юнит-тест фреймворка и концепцию Page Object. Автоматизировать следующий сценарий:
+//
+//        Открыть https://pastebin.com или аналогичный сервис в любом браузере
+//        Создать New Paste со следующими деталями:
+//        * Код: "Hello from WebDriver"
+//
+//        * Paste Expiration: "10 Minutes"
+//
+//        * Paste Name / Title: "helloweb"
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import java.util.List;
+
+
+public class PastebinTestTest extends BaseTest{
+        @Test
+        public void pastebinTest(){
+            driver.get("https://pastebin.com");
+            driver.findElement(By.id("postform-text")).sendKeys("Hello from WebDriver");
+            driver.findElement(By.id("select2-postform-expiration-container")).click();
+            List<WebElement> elements = driver.findElements(By.cssSelector(".select2-results li"));
+            elements.get(2).click();
+            driver.findElement(By.id("postform-name")).sendKeys("helloweb");
+            driver.findElement(By.cssSelector("button[type='submit']")).click();
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Assert.assertTrue(driver.getTitle().contains("helloweb"));
+            driver.quit();
+        }
+    }
