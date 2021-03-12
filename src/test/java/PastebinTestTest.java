de79dd09cef747013897b8a0a9f09a535f3669cd
Anastasiya-Klukva <Petrushka_n@mail.ru>
2021-03-12 19:53:23 +0300

    adding 10 really minutes expitation)

--- src/test/java/PastebinTestTest.java
+++ src/test/java/PastebinTestTest.java
@@ -23,12 +23,13 @@ public class PastebinTestTest extends BaseTest{
             driver.get("https://pastebin.com");
             driver.findElement(By.id("postform-text")).sendKeys("Hello from WebDriver");
             driver.findElement(By.id("select2-postform-expiration-container")).click();
+            driver.findElement(By.xpath("//li[text()='10 Minutes']")).click();
             List<WebElement> elements = driver.findElements(By.cssSelector(".select2-results li"));
             elements.get(2).click();
             driver.findElement(By.id("postform-name")).sendKeys("helloweb");
             driver.findElement(By.cssSelector("button[type='submit']")).click();
             try {
-                Thread.sleep(600000);
+                Thread.sleep(10000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
