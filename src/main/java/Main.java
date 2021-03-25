51d5c7a1fbae8d84a5f79cc5f3c9c3c1b3fb49c8
Anastasiya-Klukva <Petrushka_n@mail.ru>
2021-03-25 23:00:16 +0300

    i used to do this code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,28 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static java.time.Duration.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:\\Studies\\WebDriver\\chromedriver.exe" );
+        System.setProperty("webdriver.chrome.driver", "C:\\Studies\\WebDriver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        // получаем доступ к параметрам, заадали неявное ожидание
+        driver.manage().timeouts().implicitlyWait(ofSeconds(10));
+        //неявное ожидание
+        WebElement element = (new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.id("123"))));
         // получаем доступ к гуглу
         driver.get("https://google.com");
+        // создаем переменную, которая будет хранить ссылку на наш объект
+        WebElement input = driver.findElement(By.xpath("//form/div/input"));
+        // кликаем по нему
+        input.click();
     }
 }
--- src/test/java/test/PastebinTestTest.java
+++ src/test/java/test/PastebinTestTest.java
@@ -1,23 +1,36 @@
-// I Can Win
+package test;
+// I Can Win.TASK 1
 //        При выполнении задания необходимо использовать возможности Selenium WebDriver, юнит-тест фреймворка и концепцию Page Object. Автоматизировать следующий сценарий:
-//
 //        Открыть https://pastebin.com или аналогичный сервис в любом браузере
 //        Создать New Paste со следующими деталями:
 //        * Код: "Hello from WebDriver"
-//
 //        * Paste Expiration: "10 Minutes"
-//
 //        * Paste Name / Title: "helloweb"
+//  Bring it on. TASK 2
+//        Открыть https://pastebin.com  или аналогичный сервис в любом браузере
+//        Создать New Paste со следующими деталями:
+//        * Код:
+//        git config --global user.name  "New Sheriff in Town"
+//        git reset $(git commit-tree HEAD^{tree} -m "Legacy code")
+//        git push origin master --force
+//        * Syntax Highlighting: "Bash"
+//        * Paste Expiration: "10 Minutes"
+//        * Paste Name / Title: "how to gain dominance among developers"
+//         Сохранить paste и проверить следующее:
+//        * Заголовок страницы браузера соответствует Paste Name / Title
+//        * Синтаксис подвечен для bash
+//        * Проверить что код соответствует введенному в пункте 2
 
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import test.BaseTest;
 
 import java.util.List;
 
 
-public class PastebinTestTest extends BaseTest{
+public class PastebinTestTest extends BaseTest {
         @Test
         public void pastebinTest(){
             // получаем доступ к сайту
